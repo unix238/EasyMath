2bef50fc07468579fceb6365b56973d017d7ff38
unix238 <stayheaven1@gmail.com>
2020-03-19 01:10:15 +0600

    Edited next updates file txt

--- buble.py
+++
@@ -1,9 +0,0 @@
-
-
-def bubble(array):
-    for i in range(N-1):
-        for j in range(N-i-1):
-            if array[j] > array[j+1]:
-                buff = array[j]
-                array[j] = array[j+1]
-                array[j+1] = buff
